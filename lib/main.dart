0f857027529fdf78c64bd72e0c3b9bbb9c1c886c
mehboobiqbal44 <73356175+mehboobiqbal44@users.noreply.github.com>
2021-03-16 23:41:21 +0500

    ,

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:library_app/splash.dart';
-import 'package:library_app/ui/screens/login.dart';
 
 void main() {
   runApp(MyApp());
